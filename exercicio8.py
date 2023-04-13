ec8425bf51d524199186556ba7e1df18643a386a
unknown <darkofyther@gmail.com>
2023-04-13 09:51:14 -0300

    feita alteração na file '2' para mostrar os numeros na ordem inversa, e criada a file '8'.

--- exercicio8.py
+++ exercicio8.py
@@ -1,10 +1,9 @@
-# lê os 10 números reais do usuário e armazena em um vetor
 numeros = []
 for i in range(5):
-    numero = float(input("Digite um número real: "))
+    numero = float(input("Digite sua idade: "))
     numeros.append(numero)
 
 # exibe os números na ordem inversa
 print("Números na ordem inversa:")
-numeros.reverse()
+
 print(numeros)
