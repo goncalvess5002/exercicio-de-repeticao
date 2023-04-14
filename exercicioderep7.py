9e38dfc729e6671dbc050d93d449661494ddd74f
unknown <darkofyther@gmail.com>
2023-04-14 16:37:06 -0300

    Finalização de todos os codigos.

--- exercicioderep7.py
+++ exercicioderep7.py
@@ -1,12 +1,12 @@
 numeros = []
 for num in range(1, 6):
-    numeros.append(int(input("Digite um numero: ")))
+    numeros.append(int(input("Digite um numero:")))
 
 maiorNumero = numeros[0]
 
-cont = 1
-while cont < len(numeros):
-    if numeros[cont] > maiorNumero:
-        maiorNumero = numeros[cont]
-    cont = cont + 1
-print(' maior numero: '+ str (maiorNumero))
+contador= 1
+while contador < len(numeros):
+    if numeros[contador] > maiorNumero:
+        maiorNumero = numeros[contador]
+    contador = contador + 1
+print('maior numero:'+ str (maiorNumero))
--- exercicioderep8.py
+++ exercicioderep8.py
@@ -1,8 +1,8 @@
-numero1=float(input("digite o 1º numero: "))
-numero2=float(input("digite o 2º numero: "))
-numero3=float(input("digite o 3º numero: "))
-numero4=float(input("digite o 4º numero: "))
-numero5=float(input("digite o 5º numero: "))
-soma = numero1 + numero2 + numero3 + numero4 + numero5
-print("soma:",soma,)
-print("média:",soma/5)
+N1=float(input("digite o 1º numero:"))
+N2=float(input("digite o 2º numero:"))
+N3=float(input("digite o 3º numero:"))
+N4=float(input("digite o 4º numero:"))
+N5=float(input("digite o 5º numero:"))
+soma = N1 + N2 + N3 + N4 + N5
+print("soma:", soma)
+print("média:",soma / 5)
