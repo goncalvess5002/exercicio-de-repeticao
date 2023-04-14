19643d751d27f81aabf98daef8f8b91123154a19
unknown <darkofyther@gmail.com>
2023-04-14 10:34:24 -0300

    Feito um programa que leu um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações, também pede o estado civil e seu genero sexual e seu salário c:; cd 'c:\dev\exercicio-de-repeticao'; & 'C:\Users\14\AppData\Local\Programs\Python\Python311\python.exe' 'c:\Users\14\.vscode\extensions\ms-python.python-2023.6.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '57941' '--' 'C:\dev\exercicio-de-repeticao\exercicioderep3.py' .

---
+++ exercicioderep3.py
@@ -0,0 +1,30 @@
+sexo = ['m','f']
+estadocivil = ['s','c','v','d']
+
+while True:
+    nome = input("Digite seu nome: ")
+    if len(nome) > 3:
+        break
+    else:
+        print("O nome deve ter mais de 3 caracteres!")
+
+while True:
+    idade = int(input("Digite sua idade: "))
+    if idade >= 18 and idade <= 200:
+        break
+    else:
+        print("Você deve ter pelo menos 18 anos.")
+        
+while True:
+    genero = str(input("Digite seu genero sexual (m,f): "))
+    if genero in sexo:
+        break
+    else:
+        print("Você deve escolher um dos generos apresentados.")
+
+while True:
+    estado = str(input("Digite seu estado civil (s,c,v,d): "))
+    if estado in estadocivil:
+        break
+    else:
+        print("Você deve escolher uma das opções apresentadas acima.")
